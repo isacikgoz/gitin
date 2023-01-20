12f700866d224b09b33460600c8312cbd1f88db7
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2023-01-20 13:06:01 +0300

    bump version

--- git/repository_test.go
+++ git/repository_test.go
@@ -2,7 +2,6 @@ package git
 
 import (
 	"os"
-	"runtime"
 	"testing"
 )
 
@@ -21,18 +20,4 @@ func TestOpen(t *testing.T) {
 			t.Errorf("input: %s\n error: %s", test.input, err.Error())
 		}
 	}
-}
-
-// got form got2go tests, seems useful
-func checkFatal(t *testing.T, err error) {
-	if err == nil {
-		return
-	}
-
-	// The failure happens at wherever we were called, not here
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		t.Fatalf("Unable to get caller")
-	}
-	t.Fatalf("Fail at %v:%v; %v", file, line, err)
 }
